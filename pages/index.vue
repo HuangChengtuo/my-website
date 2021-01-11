59601e303da428ddeeb47a923adf7e505e03096f
HuangChengtuo <hct564117373@gmail.com>
2021-01-11 21:42:52 +0800

    time

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="home-page">
     <nuxt-link to="/sort-app">sort app</nuxt-link>
+    <br>
     <a-button type="primary">asdasd</a-button>
+    <br>
+    {{ now }}
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import dayjs from 'dayjs'
 
 export default Vue.extend({
   async asyncData({ $api }) {
@@ -14,8 +18,18 @@ export default Vue.extend({
   },
   data() {
     return {
-      bitcoin: {}
+      bitcoin: {},
+      timer: null,
+      now: ''
     }
+  },
+  created() {
+    this.timer = setInterval(() => {
+      this.now = dayjs().format('YYYY-MM-DD HH:mm:ss')
+    }, 1000)
+  },
+  beforeDestroy() {
+    clearInterval(this.timer)
   }
 })
 </script>
