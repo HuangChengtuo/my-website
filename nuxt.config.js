98148ecc80580cd148a4db8dd6573eafa69de27d
hct <564117373@qq.com>
2021-01-29 19:23:29 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,12 +7,10 @@ export default {
   buildModules: ['@nuxt/typescript-build'],
   // 自动注册全局组件，需要声明相应文件夹，没有声明的仍需 import 引入
   components: {
-    dirs: [
-      {
-        path: '~/components/layouts/',
-        prefix: 'Layouts'
-      }
-    ]
+    dirs: [{
+      path: '~/components/layouts/',
+      prefix: 'Layouts'
+    }]
   },
   css: ['@/static/styles/index.scss'],
   head: {
@@ -25,7 +23,7 @@ export default {
     title: '黄秤砣的主页'
   },
   modules: ['@nuxtjs/axios'],
-  plugins: ['@/plugins/api.ts', '@/plugins/antd.ts','@/plugins/utils.ts'],
+  plugins: ['@/plugins/api.ts', '@/plugins/antd.ts', '@/plugins/utils.ts'],
   proxy: {
     '/proxy': {
       target: 'https://kankanapi.bihukankan.com',
