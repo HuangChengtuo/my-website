d37fbe62f2709d4eb6e2feb7a1beb16dadbacd47
hct <564117373@qq.com>
2021-02-17 14:20:20 +0800

    test server time

--- pages/playground.vue
+++ pages/playground.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="playground" style="height: 2000px">
-    <TodayBangumi />
+    {{ time }}
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import TodayBangumi from "@/components/index/TodayBangumi.vue";
-
 
 export default Vue.extend({
-  components: { TodayBangumi }
+  async asyncData({ $formatTime }) {
+    return { time: $formatTime() }
+  }
 })
 </script>
