36c0db182af2a219bf33960aa3b7b40e214d0eb0
hct <564117373@qq.com>
2021-03-22 11:22:36 +0800

    lock

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>error page</div>
+</template>
+
+<script>
+export default {
+  layout: 'null'
+}
+</script>
+
+<style scoped>
+</style>
--- pages/playground.vue
+++ pages/playground.vue
@@ -1,18 +1,13 @@
 <template>
-  <div>
-    playground
-  </div>
+  <div>playground</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  created() {
-    console.log('created')
-    this.$api.get('https://s1.huangchengtuo.com/json/bangumi.json').then(() => {
-      console.log(this.$formatTime())
-    })
+  mounted() {
+    throw new Error('123123')
   }
 })
 </script>
