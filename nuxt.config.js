d9959348a301bc88ffde61b93805fc72bb44aca5
HuangChengtuo <hct564117373@gmail.com>
2021-03-25 23:19:34 +0800

    static

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
     baseURL: 'https://kankanapi.bihukankan.com',
     browserBaseURL: '/proxy'
   },
+  build: { publicPath: 'https://my-website-1302789012.cos.ap-shanghai.myqcloud.com' },
   buildModules: ['@nuxt/typescript-build'],
   // 自动注册全局组件，需要声明相应文件夹，没有声明的仍需 import 引入
   components: {
