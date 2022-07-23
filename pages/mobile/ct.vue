407cbbda2923c42a6c4983aaaacaed40f2c527de
HuangChengtuo <hct564117373@gmail.com>
2022-07-23 16:14:22 +0800

    雪碧图

--- pages/mobile/ct.vue
+++ pages/mobile/ct.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="ct-page">
-    <template v-for="item of 399">
+    <!-- <template v-for="item of 399">
       <img v-show="item - 1 === id" :key="item" style="width:100vw" :src="`https://s1.huangchengtuo.com/img/ct/${'1652326155109' + item - 1}.JPG`" />
-    </template>
-    <!-- <div
+    </template> -->
+    <div
       class="img"
       :style="`background-position:-${100 * (id % 100)}vw 0;background-image:url(https://s1.huangchengtuo.com/img/ct/${getBg()}.jpg)`"
     />
     <img v-show="false" src="https://s1.huangchengtuo.com/img/ct/1.jpg" />
     <img v-show="false" src="https://s1.huangchengtuo.com/img/ct/2.jpg" />
     <img v-show="false" src="https://s1.huangchengtuo.com/img/ct/3.jpg" />
-    <img v-show="false" src="https://s1.huangchengtuo.com/img/ct/4.jpg" />-->
+    <img v-show="false" src="https://s1.huangchengtuo.com/img/ct/4.jpg" />
     <el-slider style="margin:256rem 64rem" v-model="id" :show-tooltip="false" :min="0" :max="398" />
   </div>
 </template>
